24b99ac1d3cd683ef72f43f3c694d527c6e2561b
patrycjabru <patrycja.bruzdzinska@gmail.com>
2017-03-12 16:04:35 +0100

    commit1

--- lab2/xorcypherbreaker/XorCypherBreaker.cpp
+++ lab2/xorcypherbreaker/XorCypherBreaker.cpp
@@ -5,6 +5,12 @@ using namespace std;
 #include "XorCypherBreaker.h"
 #include <algorithm>
 #include <sstream>
+#include <iostream>
+
+using std::find;
+using std::vector;
+using std::string;
+
 std::string XorCypherBreaker(const std::vector<char> &cryptogram,
                             int key_length,
                             const std::vector<std::string> &dictionary){
@@ -13,9 +19,13 @@ std::string XorCypherBreaker(const std::vector<char> &cryptogram,
     int counter=1;
     int letter;
     int decryptedLetter;
+    int number=0;
+    int theBiggestNumber=0;
     for(int firstLetter=97;firstLetter<123;firstLetter++) {
         for(int secondLetter=97;secondLetter<123;secondLetter++) {
             for(int thirdLetter=97;thirdLetter<123;thirdLetter++) {
+                number=0;
+                counter=1;
                 for (char i : cryptogram) {
                     //counter%3
                     if (counter%3==1)
@@ -24,30 +34,33 @@ std::string XorCypherBreaker(const std::vector<char> &cryptogram,
                         letter=secondLetter;
                     else
                         letter=thirdLetter;
-                    decryptedLetter=letter^i;
+                    decryptedLetter=i^letter;
                     if (decryptedLetter<65 or (decryptedLetter>90 and decryptedLetter<97) or decryptedLetter>122) {
                         if (find(dictionary.begin(),dictionary.end(),word) != dictionary.end()) {
-                            ostringstream stringLetter;
-                            stringLetter << firstLetter;
-                            string str = stringLetter.str();
-                            key = key + stringLetter;
-//                            stringLetter << secondLetter;
-//                            key+=stringLetter;
-//                            stringLetter << thirdLetter;
-//                            key+=stringLetter;
-                            return key;
+                            number+=1;
                         }
                         word="";
                     }
                     else {
-                        ostringstream ss;
-                        ss << decryptedLetter;
-                        string str = ss.str();
+                        string str;
+                        str = decryptedLetter;
                         word = word + str;
                     }
                     counter+=1;
                 }
+                if (number > theBiggestNumber) {
+                    key="";
+                    char stringLetter;
+                    stringLetter = firstLetter;
+                    key+=stringLetter;
+                    stringLetter = secondLetter;
+                    key+=stringLetter;
+                    stringLetter = thirdLetter;
+                    key+=stringLetter;
+                    theBiggestNumber = number;
+                }
             }
         }
     }
+    return key;
 }
