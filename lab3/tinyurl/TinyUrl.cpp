cda83b5620da444e9d97f7c159de00f76eea9192
bodzbarb <barbara.bodzionyy@gmail.com>
2017-03-14 12:24:01 +0100

    rgd

--- lab3/tinyurl/TinyUrl.cpp
+++ lab3/tinyurl/TinyUrl.cpp
@@ -2,7 +2,28 @@
 // Created by bodzbarb on 14.03.17.
 //
 
-std::unique_ptr<TinyUrlCodec> Init();
-void NextHash(std::arrray<char, 6> *state);
-std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
-std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+#include <TinyUrl.h>
+//std::unique_ptr<TinyUrlCodec> Init();
+
+void NextHash(std::array<char, 6> *state)
+{
+    int ascii;
+
+    for(int i=4;i>=0;i++)
+    {
+        if(i==5 or state[i+1]=='0')
+        {
+            ascii=state[i];
+            ascii+=1;
+            if(ascii==58)
+                ascii=65;
+            else if(ascii==91)
+                ascii=97;
+            else if(ascii==123)
+                ascii=48;
+            state[i]=ascii;
+        }
+    }
+}
+//std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
+//std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
--- lab3/tinyurl/main.cpp
+++ lab3/tinyurl/main.cpp
@@ -2,3 +2,8 @@
 // Created by bodzbarb on 14.03.17.
 //
 
+#include <TinyUrl.h>
+
+int main() {
+    return 0;
+}
