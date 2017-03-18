36a1f98aa80779a4dcfda16110bbafb8390d7a32
patrycjabru <patrycja.bruzdzinska@gmail.com>
2017-03-18 14:22:15 +0100

    commit1

---
+++ lab3/smarttree/SmartTree.cpp
@@ -0,0 +1,20 @@
+//
+// Created by patrycja on 18.03.17.
+//
+
+#include "SmartTree.h"
+namespace datastructures {
+    std::unique_ptr<SmartTree> CreateLeaf(int value);
+
+    std::unique_ptr<SmartTree>
+    InsertLeftChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> left_subtree);
+
+    std::unique_ptr<SmartTree>
+    InsertRightChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> right_subtree);
+
+    void PrintTreeInOrder(const std::unique_ptr<SmartTree> &unique_ptr, std::ostream *out);
+
+    std::string DumpTree(const std::unique_ptr<SmartTree> &tree);
+
+    std::unique_ptr<SmartTree> RestoreTree(const std::string &tree);
+}
---
+++ lab3/smarttree/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by patrycja on 18.03.17.
+//
+
+#include <SmartTree.h>
+
+int main() {
+    return 0;
+}
