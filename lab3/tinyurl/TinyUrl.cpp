0e26d21b7621561b964158e7734d7660b6f76471
patrycjabru <patrycja.bruzdzinska@gmail.com>
2017-03-19 23:48:47 +0100

    commit1

--- lab3/tinyurl/TinyUrl.cpp
+++ lab3/tinyurl/TinyUrl.cpp
@@ -3,47 +3,81 @@
 //
 
 
-//Jakis blad wywala jak probuje testy uruchomic i nie wiem o co chodzi T.T
-//Zrobilam tak, zeby przechodzil tablice kilka razy od poczatku, bo nie wiem jak zrobic od konca.
-//Ale nie sprawdzilam czy dziala, bo nie moge testu odpalic.
-
 #include <TinyUrl.h>
-
-
 namespace tinyurl {
-    std::unique_ptr<TinyUrlCodec> Init()
-    {
+    std::unique_ptr<TinyUrlCodec> Init() {
         std::unique_ptr<TinyUrlCodec> p=std::make_unique<TinyUrlCodec>();
+//        for(int i=0;i<6;i++)
+//            p->current_hash[i]='0';
+//        std::cout << "aaaa\n" << p->current_hash[5];
         return p;
     }
-    void NextHash(std::array<char, 6> *state)
-    {
-        int ascii=0;
-        int numberOfField;
-        int counter=0;
-        int last=0;
-        for (numberOfField=5;numberOfField>-1;numberOfField--) {
-            counter=0;
-            for (auto v : *state) {
-                if (counter == numberOfField and last == 0) {
-                    ascii = v;
-                    ascii++;
-                    if (ascii == 58)
-                        ascii = 65;
-                    else if (ascii == 91)
-                        ascii = 97;
-                    else if (ascii == 123)
-                        ascii = 48;
-                    v = ascii;
-                    last = ascii;
-                }
-                counter++;
+    void NextHash(std::array<char, 6> *state) {
+        int ascii;
+        for (int i = 5; i >= 0; i--) {
+            if (i == 5 or (*state)[i + 1] == '0') {
+                ascii = (*state)[i];
+                ascii += 1;
+                if (ascii == 58)
+                    ascii = 65;
+                else if (ascii == 91)
+                    ascii = 97;
+                else if (ascii == 123)
+                    ascii = 48;
+                (*state)[i] = ascii;
             }
+            else
+                break;
         }
     }
-};
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec) {
+        for(int i=0;i<6;i++)
+            (*codec)->current_hash[i]='0';
+        std::cout << "aaaa\n" << (*codec)->current_hash[5];
+        //NextHash(&(*codec)->current_hash);
+        //std::cout << (*codec)->current_hash[0];
+        //m.emplace("d", "ddd");
+        (*codec)->url_map.emplace(url,(*codec)->current_hash);
+        //(*codec)->url_map->emplace=(*codec)->current_hash;
+        std::string encoded="";
+        for (int i=0;i<6;i++) {
+            encoded+=(*codec)->current_hash[i];
+        }
+        return encoded;
 
+    }
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash) {
+        std::map<std::string, std::array<char, 6>>::iterator it;
+        it = codec->url_map.find(hash);
+        return it->first;
+    }
+}
+//    {
+//        int ascii=0;
+//        int numberOfField;
+//        int counter=0;
+//        int last=0;
+//        for (numberOfField=5;numberOfField>-1;numberOfField--) {
+//            counter=0;
+//            for (auto v : *state) {
+//                if (counter == numberOfField and last == 0) {
+//                    ascii = v;
+//                    ascii++;
+//                    if (ascii == 58)
+//                        ascii = 65;
+//                    else if (ascii == 91)
+//                        ascii = 97;
+//                    else if (ascii == 123)
+//                        ascii = 48;
+//                    (*state)[counter] = ascii;
+//                    last = ascii;
+//                }
+//                counter++;
+//            }
+//        }
+//    }
 
+//std::unique_ptr<TinyUrlCodec> Init();
 
 //void NextHash(std::array<char, 6> *state)
 //{
@@ -110,5 +144,3 @@ namespace tinyurl {
 //
 //    }
 //}
-std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
-std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
--- lab3/tinyurl/TinyUrl.h
+++ lab3/tinyurl/TinyUrl.h
@@ -6,18 +6,20 @@
 #include <array>
 #include <memory>
 #include <iostream>
+#include <map>
 
 #ifndef JIMP_EXERCISES_TINYURL_H
 #define JIMP_EXERCISES_TINYURL_H
+namespace tinyurl {
+    struct TinyUrlCodec {
+        std::map<std::string, std::array<char, 6>> url_map;
+        std::array<char, 6> current_hash;
+    };
 
-struct TinyUrlCodec
-{
-    char tab[6];
-}TinyUrlCodec;
-
-std::unique_ptr<TinyUrlCodec> Init()
-void NextHash(std::array<char, 6> *state);
-std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
-std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+    std::unique_ptr<TinyUrlCodec> Init();
+    void NextHash(std::array<char, 6> *state);
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+}
 
 #endif //JIMP_EXERCISES_TINYURL_H
