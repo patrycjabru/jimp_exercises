03c773dc8c8b0efd125dda8d317566e3e103f578
patrycjabru <patrycja.bruzdzinska@gmail.com>
2017-04-04 12:27:15 +0200

    com

--- lab6/student/Student.cpp
+++ lab6/student/Student.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "Student.h"
+void CheckNextChar(char c, istream* is) {
+    int next_char = is->peek();
+    if (next_char != c) {
+        throw runtime_error("invalid character");
+    }
+    is->ignore();
+}
+
+//void IgnoreWhitespace(istream* is) {
+//    (*is) >> ws;
+//}
+
+double ReadNumber(istream* is) {
+    double d;
+    (*is) >> d;
+    return d;
+}
+//istream& operator>>(istream & input, StudentRepository& p){
+//    CheckNextChar("[Student {id: \"", &input);
+//    p.SetX(ReadNumber(&input));
+//    CheckNextChar(',', &input);
+//    IgnoreWhitespace(&input);
+//    p.SetY(ReadNumber(&input));
+//    CheckNextChar(')', &input);
+//    return input;      // Umożliwia cin >> a >> b >> c;
+//}
+friend istream& operator>>(istream & input, StudyYear& s){
+//    CheckNextChar('', &input);
+    s.SetYear(ReadNumber(&input));
+//    CheckNextChar(',', &input);
+//    IgnoreWhitespace(&input);
+//    p.SetY(ReadNumber(&input));
+//    CheckNextChar(')', &input);
+    return input;      // Umożliwia cin >> a >> b >> c;
+}
--- lab6/student/Student.h
+++ lab6/student/Student.h
@@ -6,15 +6,37 @@
 #define JIMP_EXERCISES_STUDENT_H
 #include <iostream>
 #include <string>
+#include <vector>
+#include <iomanip>
+#include <Student.cpp>
+#include <istream>
 using namespace std;
 class StudyYear {
-    StudyYear &operator++();
+public:
+    int year;
+    StudyYear &operator++() {
+        ++year;
+        return *this;
+    }
+    StudyYear &operator--() {
+        --year;
+        return *this;
+    }
+    void SetYear (int y) {
+        year=y;
+    }
 };
 class Student {
+public:
     string id;
     string first_name;
     string last_name;
     string program;
     StudyYear year;
 };
+class StudentRepository {
+public:
+    vector<Student> vec;
+};
+std::istream& operator>>(std::istream &is, StudyYear& s);
 #endif //JIMP_EXERCISES_STUDENT_H
