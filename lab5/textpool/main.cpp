fb8cc42eb68f59f57fffe36d2ee062f1ef2f07e0
patrycjabru <patrycja.bruzdzinska@gmail.com>
2017-04-05 21:30:43 +0200

    Merge branch 'master' of https://gitlab.com/agh-jimp2/exercises
    # Conflicts:
    #	lab5/CMakeLists.txt

--- lab5/textpool/main.cpp
+++ lab5/textpool/main.cpp
@@ -9,17 +9,16 @@ using namespace std;
 
 int main() {
     //Inicjalizacja wstępna puli za pomocą listy inicjalizacyjnej
-    TextPool pool {"abc", "efg", "hij", "klmn", "oprst", "abc"};
+    TextPool pool {"abc", "efg", "hij", "klmn", "oprst"};
 
     //wstawienie napisu do puli
-    auto s0 = pool.pool[1];
     auto s1 = pool.Intern("efg");
-    auto s3 = pool.Intern("efgh");
+
     //wstawienie kolejnego napisu do puli (w obu przypadkach nie
     //powinien się zmienić rozmiar puli)
     auto s2 = pool.Intern("efg");
-//
-    cout << (s1 == s0 ? "True" : "False") << endl; //uchwyty są tymi samymi napisami co do wartości
+
+    cout << (s1 == s2 ? "True" : "False") << endl; //uchwyty są tymi samymi napisami co do wartości
     cout << pool.StoredStringCount() << endl; // w puli jest wciąż 5 napisów
     cout << (s1.begin() == s2.begin() ? "True" : "False") << endl; //na dodatek uchywyty s1 i s2 pokazują dokładnie na ten sam napis w puli (wskaźniki są identyczne)
 }
