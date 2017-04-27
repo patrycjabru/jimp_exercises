369f66af5609e17d2b6d545f4978de553eeeef71
barbarabod <barbara.bodzionyy@gmail.com>
2017-04-27 22:31:43 +0200

    koniec

--- lab7/point3D/main.cpp
+++ lab7/point3D/main.cpp
@@ -10,14 +10,14 @@ using namespace std;
 int main() {
     Point p2d{2,4};
     Point3D p3d{1,2,3};
-    //najpierw wywołany zostanie konstruktor point, a potem point3d
-    //dla destruktorów odwrotna kolejność
+    // najpierw wywołany zostanie konstruktor point, a potem point3d
+    // dla destruktorów odwrotna kolejność
 
     cout << p2d.Distance(p3d) << endl;
-    //funkcja liczy dystans 2d, ponieważ jest wywołana dla punktu 2d; ignoruje trzecią wartość p3d
+    // funkcja liczy dystans 2d, ponieważ jest wywołana dla punktu 2d; ignoruje trzecią wartość p3d
 
     cout << p3d << endl;
-    //wypisze tylko pierwsze 2 wartości p3d, ponieważ operator << był stworzony dla klasy Point
+    // wypisze tylko pierwsze 2 wartości p3d, ponieważ operator << był stworzony dla klasy Point
 
     return 0;
 }
