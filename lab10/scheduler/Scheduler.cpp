c98b3d58df1c6a225816adf031bca73d1337c0c3
barbarabod <barbara.bodzionyy@gmail.com>
2017-05-24 20:29:40 +0200

    Merge branch 'b' of https://github.com/patrycjabru/jimp_exercises
    # Conflicts:
    #	lab10/CMakeLists.txt
    #	lab8/CMakeLists.txt

--- lab10/scheduler/Scheduler.cpp
+++ lab10/scheduler/Scheduler.cpp
@@ -3,39 +3,113 @@
 //
 
 #include "Scheduler.h"
+
 namespace academia {
+    SchedulingItem::SchedulingItem(int course_id, int teacher_id, int room_id, int time_slot, int year) {
+        this->course_id=course_id;
+        this->teacher_id=teacher_id;
+        this->room_id=room_id;
+        this->time_slot=time_slot;
+        this->year=year;
+    }
+
+    int SchedulingItem::CourseId() const {
+        return course_id;
+    }
+
+    int SchedulingItem::TeacherId() const {
+        return teacher_id;
+    }
+
+    int SchedulingItem::RoomId() const {
+        return room_id;
+    }
+
+    int SchedulingItem::TimeSlot() const {
+        return time_slot;
+    }
+
+    int SchedulingItem::Year() const {
+        return year;
+    }
 
     Schedule Scheduler::PrepareNewSchedule(const std::vector<int> &rooms,
                                            const std::map<int, std::vector<int>> &teacher_courses_assignment,
                                            const std::map<int, std::set<int>> &courses_of_year, int n_time_slots) {
-        for (auto c : courses_of_year) {
-            int numberOfCourse = c.first;
-            for (auto x : c.second) {
-                int year = x;
-                //znajdz nauczyciela
-                //znajdz sale
 
+        std::vector<std::vector<int>> vec(n_time_slots);
+        int course_id = 0, teacher_id = 0, room_id = 0, time_slot = 0, year = 0;
+        int slots = n_time_slots;
+        bool flag = false;
+        int room = 0;
+        Schedule sched;
+
+        for (auto x : teacher_courses_assignment) {
+            teacher_id = x.first;
+            for(auto y : x.second) {
+                course_id = y;
+                if(slots <= 0)
+                {
+                    slots = n_time_slots;
+                    room++;
+                }
+                if(rooms.size() < room + 1)
+                    throw NoViableSolutionFound();
+                room_id = rooms[room];
+                for (auto z : courses_of_year) {
+                    for(auto c : z.second) {
+                        if(c == y)
+                        {
+                            year = z.first;
+                            vec[slots-1].push_back(year);
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if(flag) {
+                        break;
+                    }
+                }
+                if(!flag)
+                    throw NoViableSolutionFound();
+                else
+                    flag = false;
+                sched.InsertScheduleItem(SchedulingItem{ course_id, teacher_id, room_id,slots, year } );
+                slots--;
             }
-
         }
-        return Schedule();
+
+        for(auto &a : vec)
+        {
+            std::sort(a.begin(), a.end());
+            auto p = std::unique(a.begin(), a.end());
+            if(p != a.end())
+            {
+                throw NoViableSolutionFound();
+            }
+        }
+
+        return sched;
     }
 
     Schedule Schedule::OfTeacher(int teacher_id) const {
         Schedule tmp;
-        std::copy_if (item_.begin(), item_.end(), std::back_inserter(tmp.item_), [teacher_id](SchedulingItem it ){return teacher_id==it.TeacherId();} );
+        std::copy_if (item_.begin(), item_.end(), std::back_inserter(tmp.item_),
+                      [teacher_id](SchedulingItem it ) { return teacher_id==it.TeacherId(); } );
         return tmp;
     }
 
     Schedule Schedule::OfRoom(int room_id) const {
         Schedule tmp;
-        std::copy_if (item_.begin(), item_.end(), std::back_inserter(tmp.item_), [room_id](SchedulingItem it ){return room_id==it.RoomId();} );
+        std::copy_if (item_.begin(), item_.end(), std::back_inserter(tmp.item_),
+                      [room_id](SchedulingItem it ){return room_id==it.RoomId();} );
         return tmp;
     }
 
     Schedule Schedule::OfYear(int year) const {
         Schedule tmp;
-        std::copy_if (item_.begin(), item_.end(), std::back_inserter(tmp.item_), [year](SchedulingItem it ){return (year==it.Year());} );
+        std::copy_if (item_.begin(), item_.end(), std::back_inserter(tmp.item_),
+                      [year](SchedulingItem it ){return (year==it.Year());} );
         return tmp;
     }
 
@@ -62,14 +136,5 @@ namespace academia {
                 tmp.erase(notAvailable);
         }
         return tmp;
-    }
-
-    SchedulingItem::SchedulingItem(int course_id, int teacher_id, int room_id, int time_slot, int year) {
-            this->course_id=course_id;
-            this->teacher_id=teacher_id;
-            this->room_id=room_id;
-            this->time_slot=time_slot;
-            this->year=year;
-
     }
 }
--- lab10/scheduler/Scheduler.h
+++ lab10/scheduler/Scheduler.h
@@ -1,31 +1,32 @@
 //
 // Created by moje konto on 2017-05-19.
-//
+//a
 
 #include <cstddef>
 #include <map>
 #include <vector>
 #include <algorithm>
 #include <set>
+
 #ifndef JIMP_EXERCISES_SCHEDULER_H
 #define JIMP_EXERCISES_SCHEDULER_H
 
 namespace academia {
     class SchedulingItem {
+    public:
+        SchedulingItem(int course_id,int teacher_id,int room_id,int time_slot,int year);
+
+        int CourseId() const;
+        int TeacherId() const;
+        int RoomId() const;
+        int TimeSlot() const;
+        int Year() const;
     private:
         int course_id;
         int teacher_id;
         int room_id;
         int time_slot;
         int year;
-    public:
-        SchedulingItem(int course_id,int teacher_id,int room_id,int time_slot,int year);
-        int CourseId() const { return course_id;};
-        int TeacherId() const { return teacher_id;};
-        int RoomId() const { return room_id;};
-        int TimeSlot() const { return time_slot;};
-        int Year() const { return year;};
-
     };
 
     class Schedule {
@@ -38,13 +39,24 @@ namespace academia {
         size_t Size() const;
         SchedulingItem operator[](int it) const;
         std::vector<SchedulingItem> item_;
-
     };
 
     class Scheduler {
     public:
-        Schedule PrepareNewSchedule(const std::vector<int> &rooms, const std::map<int, std::vector<int>> &teacher_courses_assignment, const std::map<int, std::set<int>> &courses_of_year, int n_time_slots);
+        Schedule PrepareNewSchedule(const std::vector<int> &rooms,
+                                    const std::map<int, std::vector<int>> &teacher_courses_assignment,
+                                    const std::map<int, std::set<int>> &courses_of_year, int n_time_slots);
+    };
 
+    class GreedyScheduler : public Scheduler {
+    public:
+        GreedyScheduler() {};
+        virtual ~GreedyScheduler() {};
+    };
+
+    class NoViableSolutionFound : public std::runtime_error {
+    public:
+        NoViableSolutionFound() : std::runtime_error("Can't create schedule") {};
     };
 }
 
