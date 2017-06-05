bc1dfbcfaf9f656da693339cc663a21148cd1532
patrycjabru <patrycja.bruzdzinska@gmail.com>
2017-06-05 18:48:57 +0200

    działa

--- lab12/academiateacherhash/TeacherHash.cpp
+++ lab12/academiateacherhash/TeacherHash.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "TeacherHash.h"
+namespace academia {
+    TeacherId::TeacherId(const int &value){
+        id_=value;
+    }
+    Teacher::Teacher(const TeacherId &id, std::string name, std::string department) {
+        id_=id;
+        name_=name;
+        department_=department;
+    }
+    bool Teacher::operator==(const Teacher &t) const {
+        return name_ == t.name_ and department_ == t.department_ and id_ == t.id_;
+    }
+    bool Teacher::operator!=(const Teacher &t) const {
+        return !(name_ == t.name_ and department_ == t.department_ and id_ == t.id_);
+    }
+    int TeacherHash::operator()(const Teacher &teacher) const {
+        return std::hash<int>{}(teacher.Id())^std::hash<std::string>{}(teacher.Name())^std::hash<std::string>{}(teacher.Department());
+    }
+};
--- lab12/academiateacherhash/TeacherHash.h
+++ lab12/academiateacherhash/TeacherHash.h
@@ -4,5 +4,36 @@
 
 #ifndef JIMP_EXERCISES_TEACHERHASH_H
 #define JIMP_EXERCISES_TEACHERHASH_H
+#include <string>
+
+namespace academia
+{
+
+    class TeacherId {
+    public:
+        TeacherId(const int &value);
+        operator int() const{ return this->id_; }
+    private:
+        int id_;
+    };
+    class Teacher
+    {
+    public:
+        Teacher(const TeacherId &id, std::string name, std:: string department);
+        TeacherId Id() const {return id_; }
+        std::string Name() const {return name_; }
+        std::string Department() const {return department_; }
+        bool operator == (const Teacher& t) const;
+        bool operator != (const Teacher& t) const;
+    private:
+        std::string name_;
+        std::string department_;
+        TeacherId id_=0;
+    };
+    class TeacherHash {
+    public:
+        int operator()(const Teacher &teacher) const;
+    };
+}
 
 #endif //JIMP_EXERCISES_TEACHERHASH_H
