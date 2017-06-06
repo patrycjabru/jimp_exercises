442f25b96a6ad92fccc2fc567f4cdaee4fd66a52
patrycjabru <patrycja.bruzdzinska@gmail.com>
2017-06-06 12:24:15 +0200

    Merge branch 'a' of https://github.com/patrycjabru/jimp_exercises
    # Conflicts:
    #	lab9/academiaserializer/CMakeLists.txt
    #	lab9/academiaserializer/Serialization.cpp
    #	lab9/academiaserializer/Serialization.h
    #	lab9/academiaserializer/main.cpp

--- tests/lab13test/ArabicToRomanTest.cpp
+++ tests/lab13test/ArabicToRomanTest.cpp
@@ -1,4 +1,39 @@
 //
 // Created by mwypych on 01.06.17.
 //
+#include "ArabicRoman.h"
+#include <iostream>
+#include <gtest/gtest.h>
+#include <MemLeakTest.h>
+
+class ArabicToRomanTest : public ::testing::Test, MemLeakTest {
+public:
+};
+
+TEST_F(ArabicToRomanTest, ArabicToRoman1) {
+    EXPECT_EQ("I", ArabicToRoman(1));
+    EXPECT_EQ("II", ArabicToRoman(2));
+    EXPECT_EQ("IV", ArabicToRoman(4));
+}
+TEST_F(ArabicToRomanTest, ArabicToRoman2) {
+    EXPECT_EQ("XXIX", ArabicToRoman(29));
+    EXPECT_EQ("L", ArabicToRoman(50));
+    EXPECT_EQ("XL", ArabicToRoman(40));
+}
+TEST_F(ArabicToRomanTest, ArabicToRoman3) {
+    EXPECT_EQ("C", ArabicToRoman(100));
+    EXPECT_EQ("D", ArabicToRoman(500));
+    EXPECT_EQ("CD", ArabicToRoman(400));
+}
+TEST_F(ArabicToRomanTest, ArabicToRoman4) {
+    EXPECT_EQ("MM", ArabicToRoman(2000));
+    EXPECT_EQ("CM", ArabicToRoman(900));
+}
+TEST_F(ArabicToRomanTest, ArabicToRoman5) {
+    EXPECT_EQ("Invalid Roman Number Value", ArabicToRoman(4000));
+    EXPECT_EQ("Invalid Roman Number Value", ArabicToRoman(-10));
+}
+TEST_F(ArabicToRomanTest, ArabicToRoman6) {
+    EXPECT_EQ("CXXXVI", ArabicToRoman(136));
+}
 
--- tests/lab13test/RomanToArabicTest.cpp
+++ tests/lab13test/RomanToArabicTest.cpp
@@ -1,4 +1,28 @@
 //
 // Created by mwypych on 01.06.17.
 //
+#include "ArabicRoman.h"
+#include "../lib/memleak/MemLeakTest.h"
+#include <iostream>
+#include <gtest/gtest.h>
 
+class RomanToArabicTest : public ::testing::Test, MemLeakTest {
+public:
+};
+
+TEST_F(RomanToArabicTest, RomanToArabic1) {
+    EXPECT_EQ(1, RomanToArabic("I"));
+    EXPECT_EQ(2, RomanToArabic("II"));
+    EXPECT_EQ(3, RomanToArabic("III"));
+}
+TEST_F(RomanToArabicTest, RomanToArabic2) {
+    EXPECT_EQ(10, RomanToArabic("X"));
+    EXPECT_EQ(19, RomanToArabic("XIX"));
+    EXPECT_EQ(47, RomanToArabic("XLVII"));
+}
+TEST_F(RomanToArabicTest, RomanToArabic3) {
+    EXPECT_EQ(136, RomanToArabic("CXXXVI"));
+}
+TEST_F(RomanToArabicTest, RomanToArabic4) {
+    EXPECT_EQ(0, RomanToArabic(""));
+}
