bafcb16f0da5ae0a29a9c1bdf59baa2f14f035cf
bodzbarb <barbara.bodzionyy@gmail.com>
2017-06-06 12:24:25 +0200

    13

---
+++ lab13/arabicroman/ArabicRoman.h
@@ -0,0 +1,13 @@
+//
+// Created by bodzbarb on 06.06.17.
+//
+
+#include <string>
+
+#ifndef JIMP_EXERCISES_ARABIC_ROMAN_H
+#define JIMP_EXERCISES_ARABIC_ROMAN_H
+
+std::string ArabicToRoman(int arabic);
+int RomanToArabic(std::string roman);
+
+#endif //JIMP_EXERCISES_ARABIC_ROMAN_H
